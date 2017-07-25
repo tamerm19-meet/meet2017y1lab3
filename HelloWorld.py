08ed8ed7d6e8df44a6766080d93de253056e0b00
tamerm19-meet <tamerm19@meet.mit.edu>
2017-07-25 12:40:37 +0300

    Stopping here

---
+++ HelloWorld.py
@@ -0,0 +1,2 @@
+print("'Hej Verden' is how you say Hello World in Danish")
+print("'こんにちは世界' is how you say Hello World in Japanese")
---
+++ RobertFrost.py
@@ -0,0 +1,8 @@
+twoRoads = "Two roads diverged in a wood, and I-"
+roadTaken = "I took the one most traveled by,"
+less = "I took the one less traveled by,"
+theDifference = "And that has made all the difference."
+
+print(twoRoads)
+print(roadTaken)
+print(theDifference)
